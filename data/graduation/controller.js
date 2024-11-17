120e8ddb012197e93d8776463d170e2c50127163
Francisco Silva <silva18franciscosilva@gmail.com>
2024-11-17 20:19:22 +0000

    1

--- data/graduation/controller.js
+++ data/graduation/controller.js
@@ -1,12 +1,10 @@
+// controllers/graduationController.js
 const Graduation = require("../../models/graduation");
 const User = require("../../models/user");
-const PDFDocument = require("pdfkit");
-const fs = require("fs");
-const path = require("path");
+const { generateDiploma } = require("../../pdfs/pdfService"); // Importando o serviço de PDF
 
 const GraduationController = {
   createGraduation,
-  generateDiploma,
   evaluateGraduation,
   getUserGraduations,
   getGraduationById,
@@ -17,20 +15,17 @@ const GraduationController = {
 // 1. Criar Graduação
 async function createGraduation(userId, level, instructorId, location, date) {
   try {
-    // Se a data não for fornecida, usa a data atual
     const graduationDate = date || new Date();
-
     const graduation = new Graduation({
       user: userId,
       level: level,
       instructor: instructorId,
       location: location,
-      date: graduationDate, // Usando a data fornecida ou a data atual
+      date: graduationDate,
     });
 
     await graduation.save();
 
-    // Após salvar a graduação, adiciona ela à lista de graduações do usuário
     const user = await User.findById(userId);
     if (!user) {
       throw new Error("Usuário não encontrado");
@@ -45,96 +40,13 @@ async function createGraduation(userId, level, instructorId, location, date) {
   }
 }
 
-async function generateDiploma(graduation) {
-  return new Promise((resolve, reject) => {
-    // Verificar se os campos necessários estão presentes
-    if (!graduation.user || !graduation.user.name) {
-      return reject(new Error("Dados do usuário estão incompletos"));
-    }
-
-    // Continuação da criação do PDF
-    const doc = new PDFDocument();
-    const dirPath = path.join(__dirname, "../../diplomas");
-    const filePath = path.join(dirPath, `${graduation._id}.pdf`);
-
-    if (!fs.existsSync(dirPath)) {
-      fs.mkdirSync(dirPath, { recursive: true });
-    }
-
-    const writeStream = fs.createWriteStream(filePath);
-    doc.pipe(writeStream);
-
-    // Adiciona conteúdo ao PDF
-    // Adiciona cabeçalho com nome do dojo
-    doc.image(path.join(__dirname, "../../assets/logo-ipp.png"), {
-      fit: [100, 100],
-      align: "center",
-      valign: "top",
-    });
-    doc
-      .fontSize(30)
-      .font("Helvetica-Bold")
-      .text("Cobra Kai Dojo", { align: "center", underline: true });
-
-    doc
-      .moveDown()
-      .fontSize(25)
-      .font("Helvetica-Bold")
-      .text(`Diploma de Graduação`, { align: "center" });
-
-    doc
-      .moveDown()
-      .fontSize(18)
-      .font("Helvetica")
-      .text(`Certificamos que o aluno ${graduation.user.name.toUpperCase()}`, {
-        align: "center",
-      })
-      .moveDown()
-      .text(
-        `Alcançou o cinto de nível ${graduation.level.toUpperCase()} com mérito.`,
-        { align: "center" }
-      )
-      .moveDown()
-      .text(`Data de Graduação: ${graduation.date.toLocaleDateString()}`, {
-        align: "center",
-      })
-      .moveDown()
-      .text(`Emitido no local: ${graduation.location}`, { align: "center" });
-
-    doc
-      .moveDown()
-      .fontSize(14)
-      .text(`Pontuação obtida: ${graduation.score}`, { align: "center" });
-
-    if (graduation.comments) {
-      doc
-        .moveDown()
-        .text(`Comentário: ${graduation.comments}`, { align: "center" });
-    }
-
-    // Adiciona assinatura fictícia
-    doc
-      .moveDown(2)
-      .fontSize(16)
-      .text("_________________________", { align: "right" })
-      .text("Sensei John Kreese", { align: "right" })
-      .text("Cobra Kai Dojo", { align: "right" });
-
-    doc.end();
-
-    writeStream.on("finish", () => resolve(filePath));
-    writeStream.on("error", (error) => reject(error));
-  });
-}
-
-// 2. Avaliar Graduação (Atualizar pontuação)
+// 2. Avaliar Graduação
 async function evaluateGraduation(id, score, comments) {
   if (score < 0 || score > 100) {
     throw new Error("Pontuação deve estar entre 0 e 100");
   }
 
   try {
-    // Buscar a graduação pelo ID
     const graduation = await Graduation.findById(id)
       .populate("user")
       .populate("instructor");
@@ -148,17 +60,15 @@ async function evaluateGraduation(id, score, comments) {
       throw new Error("Dados do usuário estão incompletos");
     }
 
-    // Atualizar a pontuação e os comentários da graduação
     graduation.score = score;
     graduation.comments = comments;
     await graduation.save();
 
-    // Atualizar o nível do usuário (belt) se a pontuação for >= 50
     if (score >= 50) {
       user.belt = graduation.level;
       await user.save();
 
-      // Gerar o diploma após a avaliação bem-sucedida
+      // Gerar diploma após avaliação bem-sucedida
       const diplomaPath = await generateDiploma(graduation);
       console.log(`Diploma gerado em: ${diplomaPath}`);
     }
@@ -203,36 +113,32 @@ async function getGraduationById(id) {
   }
 }
 
+// 5. Atualizar Graduação
 async function updateGraduation(id, score, comment = "", certificateUrl = "") {
-  // Verificar se a pontuação está dentro do intervalo permitido
   if (score < 0 || score > 100) {
     throw new Error("Pontuação deve estar entre 0 e 100");
   }
 
   try {
-    // Buscar a graduação pelo ID
     const graduation = await Graduation.findById(id);
     if (!graduation) {
       throw new Error("Graduação não encontrada");
     }
 
-    // Atualizar a pontuação, o comentário e o certificateUrl (se fornecido)
     graduation.score = score;
     graduation.comments = comment;
     if (certificateUrl) {
-      graduation.certificateUrl = certificateUrl; // Atualiza o certificateUrl, se fornecido
+      graduation.certificateUrl = certificateUrl;
     }
     await graduation.save();
 
-    // Buscar o usuário relacionado
     const user = await User.findById(graduation.user);
     if (!user) {
       throw new Error("Usuário não encontrado");
     }
 
-    // Atualizar o nível (belt) do usuário se a pontuação for >= 50
     if (score >= 50) {
-      user.belt = graduation.level; // Atualizar o belt do usuário para o level da graduação
+      user.belt = graduation.level;
       await user.save();
     }
 
---
+++ pdfs/pdfService.js
@@ -0,0 +1,89 @@
+// pdfService.js
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
+const path = require("path");
+
+async function generateDiploma(graduation) {
+  return new Promise((resolve, reject) => {
+    // Verificar se os campos necessários estão presentes
+    if (!graduation.user || !graduation.user.name) {
+      return reject(new Error("Dados do usuário estão incompletos"));
+    }
+
+    // Continuação da criação do PDF
+    const doc = new PDFDocument();
+    const dirPath = path.join(__dirname, "../diplomas");
+    const filePath = path.join(dirPath, `${graduation._id}.pdf`);
+
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath, { recursive: true });
+    }
+
+    const writeStream = fs.createWriteStream(filePath);
+    doc.pipe(writeStream);
+
+    // Adiciona conteúdo ao PDF
+    doc.image(path.join(__dirname, "../assets/logo-ipp.png"), {
+      fit: [100, 100],
+      align: "center",
+      valign: "top",
+    });
+    doc
+      .fontSize(30)
+      .font("Helvetica-Bold")
+      .text("Cobra Kai Dojo", { align: "center", underline: true });
+
+    doc
+      .moveDown()
+      .fontSize(25)
+      .font("Helvetica-Bold")
+      .text(`Diploma de Graduação`, { align: "center" });
+
+    doc
+      .moveDown()
+      .fontSize(18)
+      .font("Helvetica")
+      .text(`Certificamos que o aluno ${graduation.user.name.toUpperCase()}`, {
+        align: "center",
+      })
+      .moveDown()
+      .text(
+        `Alcançou o cinto de nível ${graduation.level.toUpperCase()} com mérito.`,
+        { align: "center" }
+      )
+      .moveDown()
+      .text(`Data de Graduação: ${graduation.date.toLocaleDateString()}`, {
+        align: "center",
+      })
+      .moveDown()
+      .text(`Emitido no local: ${graduation.location}`, { align: "center" });
+
+    doc
+      .moveDown()
+      .fontSize(14)
+      .text(`Pontuação obtida: ${graduation.score}`, { align: "center" });
+
+    if (graduation.comments) {
+      doc
+        .moveDown()
+        .text(`Comentário: ${graduation.comments}`, { align: "center" });
+    }
+
+    // Adiciona assinatura fictícia
+    doc
+      .moveDown(2)
+      .fontSize(16)
+      .text("_________________________", { align: "right" })
+      .text("O Diretor", { align: "right" })
+      .text("Cobra Kai Dojo", { align: "right" });
+
+    doc.end();
+
+    writeStream.on("finish", () => resolve(filePath));
+    writeStream.on("error", (error) => reject(error));
+  });
+}
+
+module.exports = {
+  generateDiploma,
+};
